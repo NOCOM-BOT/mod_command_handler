80ec73b47a00399db0214e2ba417a0a208350761
BadAimWeeb <badaimweeb@protonmail.com>
2022-07-20 01:11:20 +0700

    input resolving

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import CMComm from "./CMC";
 import Logger from "./Logger";
 
+import crypto from "node:crypto";
+
 let cmc = new CMComm();
 
 let logger = new Logger(cmc);
@@ -111,4 +113,101 @@ cmc.on("api:cmd_list", (call_from: string, data: any, callback: (error?: any, da
     });
 });
 
+let randomAPIKey = crypto.randomBytes(48).toString("hex");
+cmc.on(`api:${randomAPIKey}`, (call_from: string, data: {
+    calledFrom: string;
+    eventName: string;
+    eventData: any;
+}, callback: (error?: any, data?: any) => void) => {
+    if (call_from != "core") {
+        callback(false);
+        return;
+    }
 
+    if (data.eventName === "interface_message") {
+        let msg = data.eventData as {
+            content: string,
+            attachments: {
+                filename: string,
+                url: string
+            }[],
+            interfaceHandlerName: string,
+            interfaceID: number,
+            additionalInterfaceData?: any
+        };
+
+        // Parse msg.content: split command and args
+        // Note: content inside "" is considered as a single argument
+        let args = msg.content
+            .replace((/”/g), "\"")
+            .replace((/“/g), "\"")
+            .split(/((?:"[^"\\]*(?:\\[\S\s][^"\\]*)*"|'[^'\\]*(?:\\[\S\s][^'\\]*)*'|\/[^/\\]*(?:\\[\S\s][^/\\]*)*\/[gimy]*(?=\s|$)|(?:\\\s|\S))+)(?=\s|$)/)
+            .filter(function (el) {
+                return !(el == null || el == "" || el == " " || !el.replace(/\s/g, '')
+                    .length);
+            })
+            .map(function (z) {
+                return z.replace(/"/g, "");
+            });
+
+        let cmd = args.shift();
+        if (!cmd) return;
+
+        let pointed_cmd: {
+            funcName: string;
+            funcDescAPI: string;
+            namespace: string;
+            command: string;
+        } | undefined = void 0;
+        let namespaceSplit = cmd.split(":");
+        if (namespaceSplit.length === 1) {
+            // Handle default commands
+            if (!default_db_cmd[cmd]) {
+                return;
+            }
+
+            // Resolve pointer
+            let target = db_cmd
+                [default_db_cmd[cmd].pointer.split(":")[0]]
+                [default_db_cmd[cmd].pointer.split(":")[1]];
+
+            if (target) {
+                pointed_cmd = {
+                    funcName: target.funcName,
+                    funcDescAPI: target.funcDescAPI,
+                    namespace: default_db_cmd[cmd].pointer.split(":")[0],
+                    command: default_db_cmd[cmd].pointer.split(":")[1]
+                };
+            }
+        } else {
+            // Handle namespaced commands
+            let namespace = namespaceSplit[0];
+            let cmd = namespaceSplit[1];
+            if (!db_cmd[namespace]) {
+                return;
+            }
+
+            if (!db_cmd[namespace][cmd]) {
+                return;
+            }
+
+            let target = db_cmd[namespace][cmd];
+            pointed_cmd = {
+                funcName: target.funcName,
+                funcDescAPI: target.funcDescAPI,
+                namespace: namespace,
+                command: cmd
+            };
+        }
+
+        if (!pointed_cmd) return;
+
+        // Call command
+        
+    }
+});
+
+cmc.callAPI("core", "register_event_hook", {
+    callbackFunction: randomAPIKey,
+    event: "interface_message"
+});
